aaf30b888b65776b1df873e445a5f126c8368af6
Dean151 <thomas.durand.verjat@gmail.com>
2017-12-03 13:42:21 +0100

    Fixed puzzle 3-2

--- day-03.playground/Contents.swift
+++ day-03.playground/Contents.swift
@@ -44,19 +44,15 @@ enum Direction {
     }
 }
 
-func coordinate(for number: Int) -> (x: Int, y:Int) {
-    guard number > 0 else {
-        fatalError("number cannot be under 1")
-    }
-    
+func puzzle3_1(input: Int) -> Int {
     var position = (x: 0, y: 0)
     var n = 0
     var turns = 0
     
-    while n < number-1 {
+    while n < input-1 {
         let length = (turns / 2) + 1
         for _ in 0..<length {
-            if n == number-1 {
+            if n == input-1 {
                 break
             }
             n += 1
@@ -65,12 +61,7 @@ func coordinate(for number: Int) -> (x: Int, y:Int) {
         turns += 1
     }
     
-    return position
-}
-
-func puzzle3_1(input: Int) -> Int {
-    let coordinates = coordinate(for: input)
-    return abs(coordinates.x) + abs(coordinates.y)
+    return abs(position.x) + abs(position.y)
 }
 
 assert(puzzle3_1(input: 1) == 0)
@@ -78,4 +69,66 @@ assert(puzzle3_1(input: 12) == 3)
 assert(puzzle3_1(input: 23) == 2)
 assert(puzzle3_1(input: 1024) == 31)
 
-print("Distance for 3-1: \(puzzle3_1(input: input))")
+//print("Distance for 3-1: \(puzzle3_1(input: input))")
+
+struct SpiralData {
+    
+    var values = [Int:[Int:Int]]()
+    
+    func value(at coordinate: (x: Int, y: Int)) -> Int? {
+        return values[coordinate.y]?[coordinate.x]
+    }
+    
+    func sum(around coordinate: (x: Int, y: Int)) -> Int {
+        if coordinate.x == 0 && coordinate.y == 0 {
+            return 1
+        } else {
+            var offsetX = [-1, -1, -1, 0, 0, 1, 1, 1]
+            var offsetY = [-1,  0,  1, -1, 1, -1, 0, 1]
+            var sum = 0
+            for i in 0..<8 {
+                sum += value(at: (x: coordinate.x + offsetX[i], y: coordinate.y + offsetY[i])) ?? 0
+            }
+            return sum
+        }
+    }
+    
+    mutating func set(value: Int, at coordinate: (x: Int, y: Int)) {
+        if values[coordinate.y] == nil {
+            values.updateValue([coordinate.x : value], forKey: coordinate.y)
+        } else {
+            values[coordinate.y]!.updateValue(value, forKey: coordinate.x)
+        }
+    }
+}
+
+func puzzle3_2(input: Int) -> Int {
+    
+    var values = SpiralData()
+    var position = (x: 0, y: 0)
+    var n = 0
+    var turns = 0
+    
+    while n < input {
+        let length = (turns / 2) + 1
+        for _ in 0..<length {
+            if n >= input {
+                break
+            }
+            // Sommer la somme des carrés adjacents
+            n = values.sum(around: position)
+            values.set(value: n, at: position)
+            position = Direction.from(turns: turns).move(from: position)
+        }
+        turns += 1
+    }
+    
+    return n
+}
+
+assert(puzzle3_2(input: 20) == 23)
+assert(puzzle3_2(input: 100) == 122)
+assert(puzzle3_2(input: 700) == 747)
+assert(puzzle3_2(input: 800) == 806)
+
+print("First value bigger for 3-2: \(puzzle3_2(input: input))")
