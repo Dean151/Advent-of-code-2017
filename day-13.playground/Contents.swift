06b0d51821f89dda7d93334a7c259cd28e76a0f7
Thomas Durand <thomas.durand.verjat@gmail.com>
2017-12-13 12:23:59 +0100

    Improving complexity for first problem

--- day-13.playground/Contents.swift
+++ day-13.playground/Contents.swift
@@ -5,39 +5,11 @@ import Foundation
 let url = Bundle.main.url(forResource: "input", withExtension: "txt")!
 let input = try! String(contentsOf: url)
 
-class Scanner {
+struct Scanner {
     let depth: Int
     let range: Int
     
-    var currentRange = 0
-    var goingDown = true
-    
-    func move() {
-        if currentRange == range-1 {
-            goingDown = false
-        }
-        if currentRange == 0 {
-            goingDown = true
-        }
-        
-        if goingDown {
-            currentRange += 1
-        } else {
-            currentRange -= 1
-        }
-    }
-    
-    func reset() {
-        currentRange = 0
-        goingDown = true
-    }
-    
-    init(depth: Int, range: Int) {
-        self.depth = depth
-        self.range = range
-    }
-    
-    convenience init?(string: String) {
+    init?(string: String) {
         let components = string.components(separatedBy: ": ")
         guard let depth = components.first.flatMap({ Int($0) }) else {
             return nil
@@ -45,7 +17,12 @@ class Scanner {
         guard let range = components.last.flatMap({ Int($0) }) else {
             return nil
         }
-        self.init(depth: depth, range: range)
+        self.depth = depth
+        self.range = range
+    }
+    
+    func doCatch(at time: Int) -> Bool {
+        return time % ((range - 1) * 2) == 0
     }
 }
 
@@ -67,30 +44,22 @@ func firewall(for input: String) -> [Scanner?] {
     return firewall
 }
 
-func severity(for firewall: [Scanner?], delayedBy delay: Int = 0) -> (severity: Int, gotCaught: Bool) {
+func severity(for firewall: [Scanner?]) -> Int {
     var severity = 0
-    var currentDepth = -delay
-    var gotCaught = false
-    
-    // Reset scanners
-    firewall.forEach({ $0?.reset() })
+    var currentDepth = 0
     
     while currentDepth < firewall.count {
         // We get in currentDepth
-        if currentDepth >= 0, let scanner = firewall[currentDepth], scanner.currentRange == 0 {
+        if let scanner = firewall[currentDepth], scanner.doCatch(at: currentDepth) {
             // We got caught !
             severity += scanner.depth * scanner.range
-            gotCaught = true
         }
-        
-        // Scanners move then
-        firewall.forEach({ $0?.move() })
         
         // We increment currentDepth
         currentDepth += 1
     }
     
-    return (severity, gotCaught)
+    return severity
 }
 
 let example = """
@@ -100,29 +69,19 @@ let example = """
 6: 4
 """
 let exampleScanners = firewall(for: example)
-assert(severity(for: exampleScanners).severity == 24)
+assert(severity(for: exampleScanners) == 24)
 
 let inputScanners = firewall(for: input)
-print("Severity for 13-1: \(severity(for: inputScanners).severity)")
+print("Severity for 13-1: \(severity(for: inputScanners))")
 
 func minimumDelay(toGetThrough firewall: [Scanner?]) -> Int {
-    // Since "0: 3", delay = 0 is not an option
-    // As "1: 2", delay = 1 is not an option either
-    var delay = 2
+    var delay = 0
     var gotCaught = false
     
     repeat {
-        let result = severity(for: firewall, delayedBy: delay)
-        gotCaught = result.gotCaught
-        print("\(delay) - \(gotCaught) - \(result.severity)")
-        if gotCaught {
-            // Since "1: 2" for example and input, delay will be pair
-            delay += 2
-            // Since "0: 3" and "6: 4"
-            while delay % 4 == 0 || delay-6 % 12 == 0 {
-                delay += 2
-            }
-        }
+        
+        // TODO simpler algorithm
+        
     } while gotCaught
     
     return delay
